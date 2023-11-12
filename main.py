7a787d9c7c67168a4fde2f2fd5b654730fb906d5
quiquemonroy <quiquemonroy@gmail.com>
2023-11-13 00:11:12 +0100

    Beta

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def gestionar_mensajes(message):
         bot.send_message(message.from_user.id,
                          f'En el mes de {MES},\n     --Gasto Esti:--\n{data.cuentas_Esti} TOTAL:{data.gasto_esti}€\n     '
                          f'--Gasto Quique:--\n{data.cuentas_qq} TOTAL:{data.gasto_qq}€')
-        print(data.quien_debe())
+        bot.send_message(message.from_user.id,data.quien_debe())
 
     elif message.text == 'Volver al Menú':
         menu(message)
